bc3afda63284f11ba01b9f5e67123a099280a556
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2024-06-04 17:42:28 -0300

    project

---
+++ AudioConverter/Music.cs
@@ -0,0 +1,88 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace AudioConverter
+{
+    internal static class Music
+    {
+        public static async Task DownloadMusicAsync(string videoUrl, string musicNamePath)
+        {
+            YoutubeClient youtubeClient = new YoutubeClient();
+            var video = await youtubeClient.Videos.GetAsync(videoUrl);
+            var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(videoUrl);
+            var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+            if (streamInfo != null)
+            {
+                Console.WriteLine("Video Encontrado:\n");
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Título: " + video.Title);
+                Console.WriteLine("Autor: " + video.Author);
+                Console.WriteLine("Data de Publicação: " + video.UploadDate);
+                Console.WriteLine("Duração: " + video.Duration);
+                Console.WriteLine("--------------------------------");
+
+                if(musicNamePath.Length > 0)
+                {
+                    await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, musicNamePath);
+                    Console.WriteLine("\nÁudio baixado com sucesso");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("O arquivo deve ter um nome");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum video encontrado");
+            }
+        }
+
+        public static void ShowMusics(string musicsFolderPath)
+        {
+            string[] archives = Directory.GetFiles(musicsFolderPath, "*.*", SearchOption.AllDirectories);
+            foreach (string a in archives)
+            {
+                int lastBar = a.LastIndexOf(@"\");
+                string correctName = a.Substring(lastBar + 1);
+
+                Console.WriteLine(correctName);
+            }
+
+        }
+
+        public static void PlayMusic(string musicToPlay)
+        {
+            Console.Clear();
+            using (var audioOutput = new WaveOutEvent())
+            {
+                using (var audioFile = new AudioFileReader(musicToPlay))
+                {
+                    audioOutput.Init(audioFile);
+                    audioOutput.Play();
+
+                    Console.WriteLine("Musica Tocando...");
+                    while (audioOutput.PlaybackState == PlaybackState.Playing)
+                    {
+                        Console.WriteLine(".");
+                        Thread.Sleep(1000);
+                    }
+                    Console.WriteLine("A música terminou");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
--- AudioConverter/Program.cs
+++ AudioConverter/Program.cs
@@ -11,79 +11,54 @@ namespace AudioConverter
     {
         static async Task Main(string[] args)
         {
-            
-            //Download archive path
-            //Downloaded archive path
-            var outfile = @"C:\Users\rafar\OneDrive\Área de Trabalho\vscode projects\C#\AudioConverter\AudioConverter\musics\teste.mp3";
-            var outfileE = @"C:\Users\rafar\OneDrive\Área de Trabalho\vscode projects\C#\AudioConverter\AudioConverter\musics\";
-
-            Console.WriteLine("--- Baixar a música pelo Youtube ---");
-            Console.WriteLine("Insira o link da música:");
-            var videoUrl = Console.ReadLine();
-            Console.Clear();
-
-            Console.WriteLine("Insira o nome da música:");
-            string musicName = Console.ReadLine();
-            Console.Clear();
-            var path = @"C:\Users\rafar\OneDrive\Área de Trabalho\vscode projects\C#\AudioConverter\AudioConverter\musics\" + musicName + ".mp3";
-
-            //Baixar video pelo Youtube
-            YoutubeClient youtubeClient = new YoutubeClient();
-            var video = await youtubeClient.Videos.GetAsync(videoUrl);
-            var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(videoUrl);
-            var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
-            if (streamInfo != null)
+            while(true)
             {
-                Console.WriteLine("Video Encontrado:\n");
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("Título: " + video.Title);
-                Console.WriteLine("Autor: " + video.Author);
-                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1 - Baixar música");
+                Console.WriteLine("2 - Tocar música");
+                int downloadOrPlay = int.Parse(Console.ReadLine());
 
-                await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, path);
-                Console.WriteLine("\nÁudio baixado com sucesso");
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("Nenhum video encontrado");
-            }
-
-            //Lista de músicas na pasta musics
-            Console.Clear();
-            Console.WriteLine("--- Tocar a música ---");
-            Console.WriteLine("Lista de músicas disponíveis:");
-
-            string[] archives = Directory.GetFiles(outfileE, "*.*", SearchOption.AllDirectories);
-            foreach (string a in archives)
-            {
-                int lastBar = a.LastIndexOf(@"\");
-                string correctName = a.Substring(lastBar + 1);
-
-                Console.WriteLine(correctName);
-            }
-
-            //Tocar de música
-            Console.WriteLine("\nEscolha uma das músicas disponíveis para tocar: ");
-            var musicToPlay = outfileE + Console.ReadLine() + ".mp3";
-
-            Console.Clear();
-            using (var audioOutput = new WaveOutEvent())
-            {
-                using (var audioFile = new AudioFileReader(musicToPlay))
+                if (downloadOrPlay == 1)
                 {
-                    audioOutput.Init(audioFile);
-                    audioOutput.Play();
+                    Console.Clear();
 
-                    Console.WriteLine("Musica Tocando...");
-                    while (audioOutput.PlaybackState == PlaybackState.Playing)
-                    {
-                            Console.WriteLine(".");
-                            Thread.Sleep(1000);
-                    }
+                    //Baixar música
+                    Console.WriteLine("--- Baixar a música pelo Youtube ---");
+                    Console.WriteLine("Insira o link da música:");
+                    var videoUrl = Console.ReadLine();
+                    Console.Clear();
+
+                    Console.WriteLine("Insira o nome da música:");
+                    string musicName = Console.ReadLine();
+                    Console.Clear();
+
+                    var musicNamePath = @"C:\Users\rafar\OneDrive\Área de Trabalho\vscode projects\C#\mp3-player-downloader\AudioConverter\musics\" + musicName + ".mp3";
+
+                    await Music.DownloadMusicAsync(videoUrl, musicNamePath);
+                 
+                }
+                else if (downloadOrPlay == 2)
+                {
+                    Console.Clear();
+                    var musicsFolderPath = @"C:\Users\rafar\OneDrive\Área de Trabalho\vscode projects\C#\mp3-player-downloader\AudioConverter\musics\";
+
+                    //Listar músicas 
+                    Console.Clear();
+                    Console.WriteLine("--- Tocar a música ---");
+                    Console.WriteLine("Lista de músicas disponíveis:");
+
+                    Music.ShowMusics(musicsFolderPath);
+
+                    //Tocar música
+                    Console.WriteLine("\nEscolha uma das músicas disponíveis para tocar: ");
+                    var musicToPlay = musicsFolderPath + Console.ReadLine() + ".mp3";
+
+                    Music.PlayMusic(musicToPlay);
+
+                    Console.Clear();
                 }
             }
-
+            
         }
     }
 }
